a9dfaf6be87862d6b9da528d1d16f7044f308af0
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-09-25 04:45:11 +0600

    modal added by vanilla javascript

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// Humburger 
 function toggleMenu() {
   const navLinks = document.querySelector('.navright ul');
   navLinks.classList.toggle('active');
+}
+// Subscription Modal
+const modal = document.getElementById('myModal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModal = document.getElementsByClassName('close')[0];
+
+openModalBtn.onclick = function (){
+  modal.style.display = 'flex';
+}
+closeModal.onclick = function (){
+  modal.style.display = 'none';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
